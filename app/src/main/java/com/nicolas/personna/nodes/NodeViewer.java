94197ea928975fce0395425dd54dfe653e1c740d
NicolasMG <nicolas.m.greiner@gmail.com>
2019-01-27 21:26:26 +0100

    Modif AddActivity à AddPersonna et rajout de dossiers vides

--- app/src/main/java/com/nicolas/personna/nodes/NodeViewer.java
+++ app/src/main/java/com/nicolas/personna/nodes/NodeViewer.java
@@ -1,5 +1,6 @@
 package com.nicolas.personna.nodes;
 
+import android.annotation.SuppressLint;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
@@ -15,9 +16,17 @@ import org.graphstream.ui.android_viewer.AndroidViewer;
 import org.graphstream.ui.android_viewer.DefaultView;
 import org.graphstream.ui.android_viewer.util.DefaultFragment;
 import org.graphstream.ui.graphicGraph.GraphicGraph;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.Viewer.ThreadingModel;
+
+import static org.graphstream.ui.android_viewer.util.DefaultFragment.autoLayout;
+
 public class NodeViewer extends AppCompatActivity {
 
-    private DefaultView nodesView;
+    private View view;
+    private DefaultView defaultView;
+    private DefaultFragment fragment;
+    @SuppressLint("WrongViewCast")
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +34,32 @@ public class NodeViewer extends AppCompatActivity {
         setContentView(R.layout.nodes_visualisation);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
         Node node = new Node();
         GraphicGraph graph = node.getGraph();
         AndroidViewer androidViewer = new AndroidViewer(graph);
 
-        nodesView = findViewById(R.id.viewgraph);
-        nodesView.display(graph,true);
-        androidViewer.getDefaultView();
+
+        if (savedInstanceState == null) {
+            FragmentManager fm = getFragmentManager();
+
+            // find fragment or create him
+            fragment = (DefaultFragment) fm.findFragmentByTag("fragment_tag");
+            if (null == fragment) {
+                fragment = new DefaultFragment();
+                fragment.init(graph, autoLayout);
+            }
+
+            // Add the fragment in the layout and commit
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(CONTENT_VIEW_ID, fragment).commit();
+        }
 
     }
 
 
-
+/*
         protected void NodeViewer(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nodes_visualisation);
@@ -53,6 +76,6 @@ public class NodeViewer extends AppCompatActivity {
         nodesView.display(graph,true);
         androidViewer.getDefaultView();
     }
-
+*/
 
 }
